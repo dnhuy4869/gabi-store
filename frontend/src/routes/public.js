62cb9d734c0809971f14df2c6758d903eb435d51
thaivinh1235 <107470757+thaivinh1235@users.noreply.github.com>
2023-11-08 23:37:14 +0700

    .

--- frontend/src/routes/public.js
+++ frontend/src/routes/public.js
@@ -35,7 +35,6 @@ export const publicRoutes = {
         },
         { path: '/cart', element: <Cart /> },
         { path: '/blog', element: <Blog /> },
-
         { path: '/deliverInfo', element: <DeliverInfo /> },
         { path: '/story', element: <Story /> },
         { path: '/slider', element: <Slider /> },
